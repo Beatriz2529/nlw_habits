d426ef57983134debcb861985978ed2cfb129620
Alana Beatriz <alanabeatrizcourela931@gmail.com>
2023-01-20 17:14:19 -0300

    The 'add' function has been created.

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-const mensagem = "Deu certo"
-alert(mensagem)
+const form = document.querySelector("#form-habits")
+// aqui ele esta criando um novo objetos 'NLWSetup' e colocando
+//dentro da const 'nlwSetup'
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso🛑")
+    return
+  }
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
+}
+
+function save(){
+  
+}
+
+
+// const data = {
+//   run: ["01-20", "01-21", "01-22"],
+//   study: ["01-22", "01-24"],
+//   book: ["01-25"],
+// }
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
